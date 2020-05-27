2c61112a1bd24996a5a829b34db6ad8078433cb3
Daniel Q. M <danielquintaodemoraes@gmail.com>
2020-05-27 18:26:09 +0200

    coco gelado

--- scenes/3D_graphics/01_modeling/modeling.cpp
+++ scenes/3D_graphics/01_modeling/modeling.cpp
@@ -55,19 +55,20 @@ void scene_model::setup_data(std::map<std::string,GLuint>& shaders , scene_struc
     float tree_height = 2.0f;
     tronc = mesh_drawable(create_tronc(tree_r_param, tree_height));
     //Folliage cocotiers;
-    folliage = mesh_drawable(create_foliage(1.0, tree_height, 0.1));
+    folliage = mesh_drawable(create_foliage(1.0, 0, 0.1));
     //----------------
     vcl::mat3 R1 = vcl::rotation_from_axis_angle_mat3({ 0,0,1 }, 2 * 3.14f / 3);
     vcl::mat3 R2 = vcl::rotation_from_axis_angle_mat3({ 0,0,1 }, 2 * 3.14f / 6);
+    vcl::mat3 R2 = vcl::rotation_from_axis_angle_mat3({ 0,0,1 }, 2 * 3.14f / 12);
     vcl::mat3 Ry = vcl::rotation_from_axis_angle_mat3({ 0,1,0 }, 2 * 3.14f / 24);
     //----------------
     tree.add(tronc, "tronc");
-    tree.add(folliage, "feuille_1", "tronc", { 3 * tree_r_param,0,0 });
-    tree.add(folliage, "feuille_2", "tronc", { { 3 * tree_r_param,0,0 } , R1 });
-    tree.add(folliage, "feuille_3", "tronc", { { 3 * tree_r_param,0,0 } , R1 * R1 });
-    tree.add(folliage, "feuille_4", "tronc", { { 3 * tree_r_param,0,0 } , R2 * Ry });
-    tree.add(folliage, "feuille_5", "tronc", { { 3 * tree_r_param,0,0 } , R1 * R2 * Ry });
-    tree.add(folliage, "feuille_6", "tronc", { { 3 * tree_r_param,0,0 } , R1 * R1 * R2 * Ry });
+    tree.add(folliage, "feuille_1", "tronc", { 3 * tree_r_param,0,tree_height });
+    tree.add(folliage, "feuille_2", "tronc", { { 3 * tree_r_param,0,tree_height } , R1 });
+    tree.add(folliage, "feuille_3", "tronc", { { 3 * tree_r_param,0,tree_height } , R1 * R1 });
+    tree.add(folliage, "feuille_4", "tronc", { { 3 * tree_r_param,0,tree_height } , R2 * Ry });
+    tree.add(folliage, "feuille_5", "tronc", { { 3 * tree_r_param,0,tree_height } , R1 * R2 * Ry });
+    tree.add(folliage, "feuille_6", "tronc", { { 3 * tree_r_param,0,tree_height } , R1 * R1 * R2 * Ry });
     //----------------
     tree.set_shader_for_all_elements(shaders["mesh"]);
 
